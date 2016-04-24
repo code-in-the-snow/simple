bbadb5dccc0db1ed589b67a62d5816bf2877194e
code-in-the-snow <pscott_74@yahoo.com>
2016-04-24 15:36:44 -0400

    Add command line prompt for user to choose text or graphic interface.

--- SIMPLE/simple_game.py
+++ SIMPLE/simple_game.py
@@ -3,8 +3,39 @@
 # Script that calls game app and interface.
 
 from gameApp import GameApp
-from graphic_interface import GraphicInterface
+# from graphic_interface import GraphicInterface
+#
+# interface = GraphicInterface()
+# game = GameApp(interface)
+# game.run()
 
-interface = GraphicInterface()
-game = GameApp(interface)
-game.run()
+def getInterfaceChoice():
+    menu = ["Play from the command line.", "Play with a graphical interface."]
+    while True:
+        print("\n")
+        for index, item in enumerate(menu, start=1):
+            print(index, "\t", item)
+        try:
+            choice = int(input("\nChoose a game option: "))
+            if choice in [1,2]:
+                return choice
+            else: print("Choose a number from the listed game options.")
+        except ValueError:
+            print("Choose the number of a listed game option.")
+
+def executeChoice(choice):
+    if choice == 1:
+        from text_interface import TextInterface
+        interface = TextInterface()
+    else:
+        from graphic_interface import GraphicInterface
+        interface = GraphicInterface()
+    game = GameApp(interface)
+    game.run()
+
+
+def main():
+    choice = getInterfaceChoice()
+    executeChoice(choice)
+
+if __name__ == "__main__": main()
